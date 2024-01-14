4a24d28348621a94a6c2217c857f12fd5d79d252
chasepope47 <chasepope47@gmail.com>
2024-01-13 23:59:55 -0700

    Number averages

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,60 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        
+        int userNumber = -1;
+        while (userNumber != 0)
+        {
+            Console.Write("Enter a number (0 to quit): ");
+            
+            string userResponse = Console.ReadLine();
+
+            if (int.TryParse(userResponse, out userNumber))
+            {
+                if (userNumber != 0)
+                {
+                    numbers.Add(userNumber);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please enter a valid integer.");
+            }
+        }
+
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+
+        Console.WriteLine($"The sum is: {sum}");
+
+        float average = numbers.Count > 0 ? ((float)sum) / numbers.Count : 0;
+        Console.WriteLine($"The average is: {average}");
+
+        if (numbers.Count > 0)
+        {
+            int max = numbers[0];
+
+            foreach (int number in numbers)
+            {
+                if (number > max)
+                {
+                    max = number;
+                }
+            }
+
+            Console.WriteLine($"The max is: {max}");
+        }
+        else
+        {
+            Console.WriteLine("No numbers were entered, so there is no maximum.");
+        }
     }
 }
